cdc151845fc29ef5399bcd20bc5f1b4e5252c834
Raghav R V <ragvrv@gmail.com>
2015-06-05 01:56:15 +0530

    ENH introduce the model_selection module

---
+++ sklearn/model_selection/__init__.py
@@ -0,0 +1,18 @@
+from .split import (KFold, StratifiedKFold, LeaveOneLabelOut, LeaveOneOut,
+                    LeavePLabelOut, LeavePOut, ShuffleSplit,
+                    StratifiedShuffleSplit, PredefinedSplit,
+                    train_test_split, check_cv)
+
+from .validate import (cross_val_score, cross_val_predict, learning_curve,
+                       permutation_test_score, validation_curve)
+
+from .search import (GridSearchCV, RandomizedSearchCV, ParameterGrid,
+                     ParameterSampler, fit_grid_point)
+
+__all__ = ['split', 'validate', 'search', 'KFold', 'StratifiedKFold',
+           'LeaveOneLabelOut', 'LeaveOneOut', 'LeavePLabelOut', 'LeavePOut',
+           'ShuffleSplit', 'StratifiedShuffleSplit', 'PredefinedSplit',
+           'train_test_split', 'check_cv', 'cross_val_score',
+           'cross_val_predict', 'permutation_test_score', 'learning_curve',
+           'validation_curve', 'GridSearchCV', 'ParameterGrid',
+           'fit_grid_point', 'ParameterSampler', 'RandomizedSearchCV']
