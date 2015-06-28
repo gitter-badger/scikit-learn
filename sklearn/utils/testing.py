9121e48c8d769a4eb85869f54f3d1df0edd8c2ee
Raghav R V <ragvrv@gmail.com>
2015-06-28 08:19:32 +0530

    Prevent adding *SearchCV estimators from the old grid_search module

--- sklearn/utils/testing.py
+++ sklearn/utils/testing.py
@@ -600,7 +600,8 @@ def all_estimators(include_meta_estimators=False,
     path = sklearn.__path__
     for importer, modname, ispkg in pkgutil.walk_packages(
             path=path, prefix='sklearn.', onerror=lambda x: None):
-        if ".tests." in modname:
+        # Prevent adding *SearchCV estimators from the old grid_search module
+        if (".tests." in modname) or ("grid_search" in modname):
             continue
         module = __import__(modname, fromlist="dummy")
         classes = inspect.getmembers(module, inspect.isclass)
